ac48b671102eac3fbc8294c846205f8a1898b082
Lia <pavlova_li@ukr.net>
2023-11-09 00:06:47 +0200

    branches & loops

---
+++ js/02-switch.js
@@ -0,0 +1,92 @@
+/*
+ * Напиши скрипт вибору вартості готелю за кількістю зірок.
+ * 1 - 20$, 2 - 30$, 3 - 50$, 4 - 70$, 5 - 120$
+ *
+ * Якщо у змінній stars присутнє щось окрім чисел 1-5, виведи рядок
+ * 'Такої кількості зірок немає'
+ */
+
+// const stars = 1;
+// let price;
+
+// if (stars === 1) {
+//   price = 20;
+// } else if (stars === 2) {
+//   price = 30;
+// } else if (stars === 3) {
+//   price = 50;
+// } else if (stars === 4) {
+//   price = 70;
+// } else if (stars === 5) {
+//   price = 120;
+// } else {
+//   console.log('Такої кількості зірок немає');
+// }
+
+// switch (stars) {
+//   case 1:
+//     price = 20;
+//     break;
+
+//   case 2:
+//     price = 30;
+//     break;
+
+//   case 3:
+//     price = 50;
+//     break;
+
+//   case 4:
+//     price = 70;
+//     break;
+
+//   case 5:
+//     price = 120;
+//     break;
+
+//   default:
+//     console.log('Такої кількості зірок немає');
+// }
+
+// console.log(price);
+
+/*
+ * Напиши скрипт вибору вартості готелю за кількістю зірок.
+ * 1,2 - 20$, 3,4 - 30$, 5 - 120$
+ */
+
+// const stars = 1;
+// let price;
+
+// if (stars === 1 || stars === 2) {
+//   price = 20;
+// } else if (stars === 3 || stars === 4) {
+//   price = 30;
+// } else if (stars === 5) {
+//   price = 120;
+// } else {
+//   console.log('Такої кількості зірок немає');
+// }
+
+// console.log(price);
+
+// switch (stars) {
+//   case 1:
+//   case 2:
+//     price = 20;
+//     break;
+
+//   case 3:
+//   case 4:
+//     price = 30;
+//     break;
+
+//   case 5:
+//     price = 120;
+//     break;
+
+//   default:
+//     console.log('Такої кількості зірок немає');
+// }
+
+// console.log(price);
